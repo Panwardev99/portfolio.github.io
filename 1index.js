e836302007adcfc5158841f86428ecc13fa5f84a
PanwarDev09 <108581999+Panwardev99@users.noreply.github.com>
2022-07-03 00:35:24 +0530

    Added new files

---
+++ 1index.js
@@ -0,0 +1,10 @@
+let myArr = ["car", "Camera", 34, null, true];
+
+// //console.log(myArr.length);
+// console.log(myArr.pop())
+myArr.push("dev");
+console.log(myArr);
+console.log(myArr.shift())
+const newLen = myArr.unshift("dev")
+console.log(newLen);
+console.log(myArr)
